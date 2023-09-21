bd8fca522ee4532c11a7a2012a89b1ba95304512
quinn-skidmore <qman30austin@gmail.com>
2023-09-21 10:29:54 -0500

    Buttons add buttons (currently broken cause of NewButtonLayout fake class

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
 
 class BoxLayoutExample(BoxLayout):
-    pass
+    def add_button(self):
+        new_button = Button(text="new button")
+        self.new.add_widget(new_button)
+        print("Butt-on")
+
+    def remove_button(self):
+        self.new.remove_button(self)
+        print("Butt-off")
 
 
 class MainWidget(Widget):
     pass
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "lr-tb"
 
 
 class WidgetLayoutApp(App):
