e574c441f27978716ffe2073d0a5e1aedeeba488
quinn-skidmore <qman30austin@gmail.com>
2023-09-26 10:33:52 -0500

    Added a bunch of stuff and finished the layout

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
-
+class LayoutAssignment(BoxLayout):
+    pass
+class GridLayoutExample(GridLayout):
+    pass
 class BoxLayoutExample(BoxLayout):
     def add_button(self):
-        new_button = Button(text="new button")
-        self.new.add_widget(new_button)
+
         print("Butt-on")
 
     def remove_button(self):
-        self.new.remove_button(self)
+        self.remove_button(self)
         print("Butt-off")
 
 
@@ -21,9 +24,20 @@ class MainWidget(Widget):
 
 
 class StackLayoutExample(StackLayout):
-    def __init__(self, **kwargs):
+    def __init__(self, last_created_button, **kwargs):
         super().__init__(**kwargs)
+        self.last_created_button = last_created_button
         self.orientation = "lr-tb"
+        b = Button("Add Button", on_press=self.add_button(self))
+
+    def add_button(self):
+        new_button = Button(text="new button")
+        new_b = self.add_widget(new_button)
+        new_b.bind(on_press=self.remove_button())
+        self.last_created_button = new_b
+
+    def remove_button(self):
+        self.remove_widget(self.last_created_button)
 
 
 class WidgetLayoutApp(App):
